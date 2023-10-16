c4afa208f4c859c21dce8c4dda0224db92d3c1b9
Taylor Hodge <j.taylor.hodge@gmail.com>
2023-10-16 16:40:24 -0700

    wip: broken pipeline

--- build_db.py
+++ build_db.py
@@ -1,13 +1,12 @@
-from datetime import timezone
-import httpx
 import git
+import httpx
 import os
 import pathlib
-from urllib.parse import urlencode
 import sqlite_utils
-from sqlite_utils.db import NotFoundError
 import time
-from datasette.utils import tilde_encode
+
+from datetime import timezone
+from sqlite_utils.db import NotFoundError
 
 root = pathlib.Path(__file__).parent.resolve()
 
@@ -38,14 +37,17 @@ def build_database(repo_path):
     all_times = created_changed_times(repo_path)
     db = sqlite_utils.Database(repo_path / "til.db")
     table = db.table("til", pk="path")
+
     for filepath in root.glob("*/*.md"):
         fp = filepath.open()
         title = fp.readline().lstrip("#").strip()
         body = fp.read().strip()
         path = str(filepath.relative_to(root))
+        slug = filepath.stem
         url = "https://github.com/jthodge/til/blob/main/{}".format(path)
-        # Do we need to render the markdown?
         path_slug = path.replace("/", "_")
+        topic = path.split("/")[0]
+
         try:
             row = table.get(path_slug)
             previous_body = row["body"]
@@ -53,35 +55,44 @@ def build_database(repo_path):
         except (NotFoundError, KeyError):
             previous_body = None
             previous_html = None
+
         record = {
             "path": path_slug,
-            "topic": path.split("/")[0],
+            "slug": slug,
+            "topic": topic,
             "title": title,
             "url": url,
             "body": body,
         }
+
         if (body != previous_body) or not previous_html:
             retries = 0
+            response = None
+
             while retries < 3:
                 headers = {}
                 if os.environ.get("GITHUB_TOKEN"):
                     headers = {
                         "authorization": "Bearer {}".format(os.environ["GITHUB_TOKEN"])
                     }
+
                 response = httpx.post(
                     "https://api.github.com/markdown",
                     json={
-                        # mode=gfm would expand #13 issue links and suchlike
                         "mode": "markdown",
                         "text": body,
                     },
                     headers=headers,
                 )
+
                 if response.status_code == 200:
                     record["html"] = response.text
                     print("Rendered HTML for {}".format(path))
                     break
+                elif response.status_code == 401:
+                    assert False, "401 Unauthorized returned from GitHub API when rendering markdown"
                 else:
+                    print(response.status_code, response.headers)
                     print("  sleeping 60s")
                     time.sleep(60)
                     retries += 1
@@ -89,6 +100,7 @@ def build_database(repo_path):
                 assert False, "Could not render {} - last response was {}".format(
                     path, response.headers
                 )
+
         record.update(all_times[path])
         with db.conn:
             table.upsert(record, alter=True)
