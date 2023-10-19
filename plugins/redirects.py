b97c33edc7dade0c91962ba1a055ef2d64b2e06d
Taylor Hodge <j.taylor.hodge@gmail.com>
2023-10-19 12:55:49 -0700

    wip: update redirect regex

--- plugins/redirects.py
+++ plugins/redirects.py
@@ -4,10 +4,16 @@ from datasette import hookimpl, Response
 @hookimpl
 def register_routes():
     return (
+        # (
+        #     r"^/til/til/(?P<topic>[^_]+)_(?P<slug>[^\.]+)\.md$",
+        #     lambda request: Response.redirect(
+        #         "/{topic}/{slug}".format(**request.url_vars), status=301
+        #     ),
+        # ),
         (
-            r"^/til/til/(?P<topic>[^_]+)_(?P<slug>[^\.]+)\.md$",
+            r"^/(?P<topic>[^_]+)/(?P<slug>[^\.]+)",
             lambda request: Response.redirect(
-                "/{topic}/{slug}".format(**request.url_vars), status=301
+                "/til/til/{topic}_{slug}>~2Emd$".format(**request.url_vars), status=301
             ),
         ),
     )
