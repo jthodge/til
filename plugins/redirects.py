b0917a524b4555f1bec560577536922a933ea2dc
Taylor Hodge <j.taylor.hodge@gmail.com>
2023-10-19 12:58:48 -0700

    Revert "wip: update redirect regex"
    This reverts commit b97c33edc7dade0c91962ba1a055ef2d64b2e06d.

--- plugins/redirects.py
+++ plugins/redirects.py
@@ -4,16 +4,10 @@ from datasette import hookimpl, Response
 @hookimpl
 def register_routes():
     return (
-        # (
-        #     r"^/til/til/(?P<topic>[^_]+)_(?P<slug>[^\.]+)\.md$",
-        #     lambda request: Response.redirect(
-        #         "/{topic}/{slug}".format(**request.url_vars), status=301
-        #     ),
-        # ),
         (
-            r"^/(?P<topic>[^_]+)/(?P<slug>[^\.]+)",
+            r"^/til/til/(?P<topic>[^_]+)_(?P<slug>[^\.]+)\.md$",
             lambda request: Response.redirect(
-                "/til/til/{topic}_{slug}>~2Emd$".format(**request.url_vars), status=301
+                "/{topic}/{slug}".format(**request.url_vars), status=301
             ),
         ),
     )
